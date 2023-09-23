8e8e421e0bc7f9b9c57cad2871f56a4cf439cb29
Lacey Washington <lacey.m.washington@gmail.com>
2023-09-23 12:55:27 -0500

    added functional components

--- src/Components/Results/index.jsx
+++ src/Components/Results/index.jsx
@@ -1,13 +1,10 @@
-import React from 'react';
 
-class Results extends React.Component {
-  render() {
-    return (
-      <section>
-        <pre>{this.props.data ? JSON.stringify(this.props.data, undefined, 2) : null}</pre>
-      </section>
-    );
-  }
+const Results = ({data}) => {
+  return (
+    <section>
+    <pre>{data ? JSON.stringify(data, undefined, 2) : null}</pre>
+  </section>
+  )
 }
 
-export default Results;
+export default Results
