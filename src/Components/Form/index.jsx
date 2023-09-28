aa0321892f7e90ee331f8e1e3381bd9c5d3399aa
Lacey Washington <lacey.m.washington@gmail.com>
2023-09-27 23:17:13 -0500

    starter code

--- src/Components/Form/index.jsx
+++ src/Components/Form/index.jsx
@@ -1,31 +1,49 @@
+import { useState } from 'react';
 import './Form.scss';
 
-export const Form = ({handleApiCall}) => {
+export const Form = ({handleApiCall, method}) => {
+  const showTextArea = (method === 'get' || method === 'delete') ? true : false
   
+const [formData, setFormData] = useState({
+  method: '',
+  url: ''
+});
+ 
+const [showTextArea, setShowTextArea] = useState(true);
+
   const handleSubmit = e => {
     e.preventDefault();
-    const formData = {
-      method:'GET',
-      url: 'https://pokeapi.co/api/v2/pokemon',
-    };
     handleApiCall(formData);
   }
+
+  const handleClick = e => {
+    setFormData({...formData, method: e.target.id})
+  }
+
+  const handleOnChange = e => {
+    setFormData({...formData, url: e.target.value })
+  }
+
+
 
   return (
     <>
     <form onSubmit={handleSubmit}>
       <label >
         <span>URL: </span>
-        <input name='url' type='text' />
+        <input name='url' type='text' value={formData.url} onChange={handleOnChange}/>
         <button type="submit">GO!</button>
       </label>
       <label className="methods">
-        <span id="get">GET</span>
-        <span id="post">POST</span>
-        <span id="put">PUT</span>
-        <span id="delete">DELETE</span>
+        <span id="get" onClick={handleClick}>GET</span>
+        <span id="post" onClick={handleClick}>POST</span>
+        <span id="put"onClick={handleClick}>PUT</span>
+        <span id="delete"onClick={handleClick}>DELETE</span>
       </label>
     </form>
+
+
+
   </>
   )
 }
